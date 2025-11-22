e5d259b8d60d17e275bb009f67d51e43bb10266d
Din-Rasy <rasy.gic.itc@gmail.com>
2025-11-22 22:59:58 +0700

    Added AboutUs page and Contact Page

--- Perfume-E_commerce/src/views/customer/AboutUs.vue
+++ Perfume-E_commerce/src/views/customer/AboutUs.vue
@@ -2,38 +2,30 @@
 import Navbar from '@/components/layout/Navbar.vue'
 import Footer from '@/components/layout/Footer.vue'
 </script>
-
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden bg-white">
-
     <header class="bg-white z-50">
       <Navbar />
     </header>
-
     <div class="pt-24 px-6 text-sm text-gray-600 flex items-center gap-1">
       <RouterLink 
         to="/" class="luxurious-roman-regular text-gray-600 hover:text-[#280559] hover:underline cursor-pointer">
         Home
       </RouterLink>
-
       <span>/</span>
-
       <RouterLink
         to="/aboutus" class="luxurious-roman-regular text-gray-800 hover:text-[#280559] hover:underline cursor-pointer">
         About Us
       </RouterLink>
     </div>
-
     <section
       class="relative w-full h-[70vh] bg-cover bg-center flex items-center justify-center mt-8 rounded-xl overflow-hidden"
       style="background-image: url('/Image/AboutPage/DR.D_candid_shot 1.png')">
       <div class="absolute inset-0 backdrop-blur-md bg-white/30"></div>
-
       <div class="relative z-10 text-center px-10 py-12 max-w-3xl">
         <h1 class="luxurious-roman-regular font-bold text-4xl lg:text-5xl xl:text-6xl text-[#280559] leading-tight mb-4">
           About Us
         </h1>
-
         <p class="luxurious-roman-regular text-black font-light text-lg lg:text-xl leading-relaxed">
           At Scent Haven, we believe that perfumes are more than just scents.
           They are expressions of individuality and style. Our passion for
@@ -41,15 +33,12 @@ import Footer from '@/components/layout/Footer.vue'
           personality and elegance.
         </p>
       </div>
-
     </section>
-
     <section class="w-full py-20 bg-white flex justify-center px-6">
       <div class="max-w-3xl text-center space-y-6">
         <h2 class="luxurious-roman-regular font-bold text-3xl lg:text-4xl xl:text-5xl text-[#280559] leading-tight">
           Our Story
         </h2>
-
         <p class="luxurious-roman-regular font-light text-back text-lg lg:text-xl leading-relaxed">
           Scent Haven Perfumes was founded by a group of fragrance enthusiasts
           with a shared vision to create a haven for perfume lovers seeking
@@ -59,7 +48,6 @@ import Footer from '@/components/layout/Footer.vue'
         </p>
       </div>
     </section>
-
     <section class="w-full">
       <img
         src="/Image/AboutPage/top of the brand 1.png"
@@ -67,15 +55,11 @@ import Footer from '@/components/layout/Footer.vue'
         class="w-full object-cover"
       />
     </section>
-
     <section class="w-full py-20 bg-white flex flex-col items-center px-6">
-      
       <h2 class="luxurious-roman-regular font-bold text-3xl lg:text-4xl xl:text-5xl text-center text-[#280559] leading-tight mb-10">
         What Makes Us Unique
       </h2>
-
       <div class="grid grid-cols-1 md:grid-cols-3 gap-10 max-w-6xl">
- 
         <div class=" space-y-4">
           <h3 class="luxurious-roman-regular font-bold text-xl lg:text-2xl text-[#280559]">
             Personalized Service
@@ -84,7 +68,6 @@ import Footer from '@/components/layout/Footer.vue'
             We understand that choosing the perfect scent is a deeply personal experience. Our team of fragrance experts is always ready to assist you in finding a fragrance that complements your personality and style. Whether you’re exploring new scents or seeking to rediscover an old favorite, we’re here to guide you every step of the way.
           </p>
         </div>
-
         <div class=" space-y-4">
           <h3 class="luxurious-roman-regular font-bold text-xl lg:text-2xl text-[#280559]">
             High Quality Ingredients
@@ -93,7 +76,6 @@ import Footer from '@/components/layout/Footer.vue'
             We believe that the key to an extraordinary scent lies in the quality of ingredients. That's why we collaborate with expert perfumes who source the finest and ethically sourced materials from around the world. We never compromise on the quality of our products, ensuring along lasting and luxurious experience.
           </p>
         </div>
-
         <div class=" space-y-4">
           <h3 class="luxurious-roman-regular font-bold text-xl lg:text-2xl text-[#280559]">
             Locally Inspired
@@ -102,11 +84,7 @@ import Footer from '@/components/layout/Footer.vue'
             Our perfumes are meticulously crafted to reflex the culture heritage, traditions, and landscapes of various regions. From the vibrant streets of Marrakech to the serene cheery blossom gardens of Kyoto, each fragrance tells a unique story that resonates with its origin.
           </p>
         </div>
-
       </div>
-
-      
-
     </section>
     <div class=" w-full text-left self-start pl-4 lg:pl-8">
       <p class="luxurious-roman-regular font-light text-black text-lg leading-relaxed mt-10  ">
--- Perfume-E_commerce/src/views/customer/Contact.vue
+++ Perfume-E_commerce/src/views/customer/Contact.vue
@@ -5,69 +5,49 @@ import Footer from '@/components/layout/Footer.vue'
 
 <template>
   <div class="flex flex-col min-h-screen overflow-hidden bg-white">
-
     <header class="bg-white z-50">
       <Navbar />
     </header>
-
     <div class="pt-24 px-6 text-sm text-gray-600 flex items-center gap-1">
       <RouterLink to="/" class="luxurious-roman-regular text-gray-600 hover:text-[#280559] hover:underline cursor-pointer">
         Home
       </RouterLink>
-
       <span>/</span>
-
       <RouterLink
         to="/contact" class="luxurious-roman-regular text-gray-800 hover:text-[#280559] hover:underline cursor-pointer">
         Contact
       </RouterLink>
     </div>
-
     <section class="w-full text-center mt-10 mb-10">
       <h1 class="luxurious-roman-regular text-4xl lg:text-5xl text-[#000] tracking-wide">
         CONTACT SCENT HAVEN
       </h1>
     </section>
-
     <section class="w-full flex flex-col lg:flex-row justify-center gap-12 px-6 mb-20">
-
-   
       <div class="max-w-md space-y-5 luxurious-roman-regular">
-
         <h2 class="text-2xl font-semibold">Get in Touch</h2>
-
         <p class="text-gray-600">
           Have a question or feedback? We’d love to hear from you.
           Reach out using the info below or fill out the form.
         </p>
-
         <div class="space-y-4 text-gray-700 luxurious-roman-regular">
-
           <div class="flex items-center gap-3">
             <img src="/Image/Contact/streamline_phone-solid.png" class="w-6 h-6" />
             <span>+855 99 623 668</span>
           </div>
-
           <div class="flex items-center gap-3">
             <img src="/Image/Contact/material-symbols_mail.png" class="w-6 h-6" />
             <span>ScentHaven@gmail.com</span>
           </div>
-
           <div class="flex items-center gap-3">
             <img src="/Image/Contact/mdi_location.png" class="w-6 h-6" />
             <span>644st, Samdech Monireth Blvd(217), Phnom Penh, Cambodia</span>
           </div>
-
           <div class="flex items-center gap-3">
-            <img 
-              src="/Image/Contact/image 335.png" 
-              class="w-6 h-6"
-            />
+            <img src="/Image/Contact/image 335.png" class="w-6 h-6"/>
             <span>https://t.me/+ScentHaven</span>
           </div>
-
         </div>
-
         <div>
           <iframe
             class="w-full h-64 rounded-xl"
@@ -77,9 +57,7 @@ import Footer from '@/components/layout/Footer.vue'
           ></iframe>
         </div>
       </div>
-
       <div class="w-full max-w-lg luxurious-roman-regular space-y-4">
-        
         <div class="flex gap-4">
           <input
             type="text"
@@ -92,31 +70,25 @@ import Footer from '@/components/layout/Footer.vue'
             class="w-1/2 px-4 py-3 border border-gray-300 rounded-lg focus:ring-[#280559] focus:ring-1"
           />
         </div>
-
         <input
           type="text"
           placeholder="Subject"
           class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-[#280559] focus:ring-1"
         />
-
         <textarea
           placeholder="Message"
           rows="5"
           class="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-[#280559] focus:ring-1"
         ></textarea>
-
         <button
           class="mt-2 px-6 py-3 bg-[#280559] text-white rounded-lg luxurious-roman-regular hover:bg-[#3a067e] transition-all"
         >
           Send Message
         </button>
-
       </div>
     </section>
-
     <footer class="bg-gray-50 z-50 mt-auto">
       <Footer />
     </footer>
-
   </div>
 </template>

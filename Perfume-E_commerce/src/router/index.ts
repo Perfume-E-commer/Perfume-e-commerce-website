23d03c720525029c3ca0bee78e3a9218a9ab284c
Dararith <louchumdararith02@gmail.com>
2026-01-03 14:07:04 +0700

    Push back to github

--- Perfume-E_commerce/src/router/index.ts
+++ Perfume-E_commerce/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
-import authService from '@/Services/authService'
+import authService from '@/services/authService'
 const routes: RouteRecordRaw[] = [
   {
     path: '/:pathMatch(.*)*',

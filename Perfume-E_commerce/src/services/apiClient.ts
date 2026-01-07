8b61edba6109d80c5804fe9620a7e0fe8d90eefa
LOU Chumdararith <louchumdararith02@gmail.com>
2026-01-07 13:18:49 +0700

    Partially done with dashboard

--- Perfume-E_commerce/src/services/apiClient.ts
+++ Perfume-E_commerce/src/services/apiClient.ts
@@ -11,7 +11,7 @@ const apiClient = axios.create({
 
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('authToken')
+    const token = localStorage.getItem('token') || localStorage.getItem('authToken');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }

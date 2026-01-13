10d61cb009970cb31b75efac700f060efd6acb82
Dararith <louchumdararith02@gmail.com>
2026-01-13 21:47:28 +0700

    feat: Add addAddress and addCreditCard methods to userService

--- Perfume-E_commerce/src/services/userService.ts
+++ Perfume-E_commerce/src/services/userService.ts
@@ -1,15 +1,44 @@
-import apiClient from './apiClient'
+import api from './apiClient'
 
-export default {
-  getProfile() {
-    return apiClient.get('/users/profile')
-  },
-  updateProfile(data: any) {
-    return apiClient.put('/users/profile', data)
-  },
-  uploadAvatar(file: File) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve('https://i.pravatar.cc/150?img=3'), 1000)
-    })
-  },
+export interface Address {
+  houseNumber: string;
+  street: string;
+  village: string;
+  community: string;
+  district: string;
+  city: string;
+  zipCode: string;
+  fullName: string;
+  phoneNumber?: string;
+  type?: string;
+  isDefault?: boolean;
 }
+
+export interface CreditCard {
+  id?: string;
+  cardHolderName: string;
+  cardNumber: string; 
+  expirationDate: string;
+  cvv: string;
+  cardType?: string;
+}
+
+class UserService {
+  getProfile() {
+    return api.get('/users/profile');
+  }
+
+  updateProfile(data: any) {
+    return api.put('/users/profile', data);
+  }
+
+  addAddress(address: Address) {
+    return api.post('/users/address', address);
+  }
+
+  addCreditCard(card: CreditCard) {
+    return api.post('/users/card', card);
+  }
+}
+
+export default new UserService();

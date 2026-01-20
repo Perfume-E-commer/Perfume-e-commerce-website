e9204a2ef3c8669a6ad6a88c921b1b9b7abbbb3f
Dararith <louchumdararith02@gmail.com>
2026-01-21 06:20:39 +0700

    refactor: cleanup OrderHistory to use OrderCard component and filter delivered orders

--- Perfume-E_commerce/src/views/customer/OrderHistory.vue
+++ Perfume-E_commerce/src/views/customer/OrderHistory.vue
@@ -26,21 +26,23 @@ const historyOrders = computed(() => {
 <template>
   <div class="flex flex-col min-h-screen bg-gray-50">
     <main class="flex-grow w-full">
+      
       <div v-if="loading" class="flex justify-center py-20">
         <div class="animate-spin rounded-full h-12 w-12 border-b-2 border-[#280559]"></div>
       </div>
 
       <div v-else class="space-y-6">
+        <div v-if="historyOrders.length === 0" class="text-center py-12 text-gray-500">
+          <p>No order history found.</p>
+        </div>
+
         <OrderCard 
           v-for="order in historyOrders" 
           :key="order.id" 
           :order="order" 
         />
-        
-        <div v-if="historyOrders.length === 0" class="text-center py-10 text-gray-400">
-           No order history found.
-        </div>
       </div>
+
     </main>
   </div>
 </template>
